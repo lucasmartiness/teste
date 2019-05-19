7ae4248a49e2c6f2583f9da24031c99ad5926db7
lucasmartines <lucas.alcantarilla97@outlook.com>
2019-05-18 23:36:22 -0300

    Adicionado a linha 3

--- exec/main.lua
+++ exec/main.lua
@@ -1,4 +1,5 @@
 function love.draw()
 	love.graphics.print("Master Of Hello World!", 400, 300)
-	love.graphics.print("Oi!", 500, 400)
+	love.graphics.print("Oi!", 400, 400)
+	love.graphics.print("Oi!", 400, 500)
 end
